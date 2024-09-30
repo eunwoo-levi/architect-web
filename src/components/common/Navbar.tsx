0009a6726b807a6e7f00b33e9749a823b421cde9
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-01 01:42:10 +0900

    fix:delete unused code - linkClassName

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,9 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import NavToggle from "./NavToggle";
 
-const linkClassName =
-  "text-md font-semibold hover:scale-105 transition-transform";
-
 export default function Navbar() {
   return (
     <nav className="border w-full min-h-[60px] flex justify-between items-center px-4 md:px-6 py-2 shadow-sm">
