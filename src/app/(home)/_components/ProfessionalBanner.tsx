64fa23fdd99aef8893fc2d0ae61c8f13728bdac6
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-01 06:14:15 +0900

    refactor: about page

--- src/app/(home)/_components/ProfessionalBanner.tsx
+++ src/app/(home)/_components/ProfessionalBanner.tsx
@@ -7,7 +7,7 @@ import { IconType } from "react-icons";
 
 interface BannerItemProps {
   icon: IconType;
-  title: string;
+  title: string[];
   description: string;
 }
 
@@ -16,16 +16,23 @@ const BannerItem: React.FC<BannerItemProps> = ({
   title,
   description,
 }) => (
-  <div className="flex flex-col items-center p-4 text-center">
+  <div className="flex flex-col items-center p-5 text-center">
     <Icon className="w-12 h-12 mb-2 text-blue-600" />
-    <h3 className="text-lg font-semibold mb-1">{title}</h3>
+    <h3 className="text-lg font-semibold mb-1 leading-tight">
+      {title.map((line, index) => (
+        <React.Fragment key={index}>
+          {line}
+          {index < title.length - 1 && <br />}
+        </React.Fragment>
+      ))}
+    </h3>
     <p className="text-sm text-gray-600">{description}</p>
   </div>
 );
 
 interface Service {
   icon: IconType;
-  title: string;
+  title: string[];
   description: string;
 }
 
@@ -33,22 +40,34 @@ const ProfessionalBanner: React.FC = () => {
   const services: Service[] = [
     {
       icon: LuBuilding2,
-      title: "건축설계",
+      title: ["건축설계"],
       description: "전문적인 설계 서비스",
     },
     {
       icon: FaHelmetSafety,
-      title: "공사 감리",
+      title: ["공사 감리"],
       description: "철저한 현장 관리",
     },
     {
       icon: LuClipboardCheck,
-      title: "건축물 점검",
+      title: ["건축물", "점검"],
       description: "안전을 최우선으로",
     },
-    { icon: LuSchool, title: "학교공간혁신", description: "대구교육청 전문가" },
-    { icon: SlBadge, title: "전문 자격", description: "대한건축사협회 정회원" },
-    { icon: IoMdTime, title: "풍부한 경험", description: "경력 20년 이상" },
+    {
+      icon: LuSchool,
+      title: ["학교공간혁신"],
+      description: "대구교육청 전문가",
+    },
+    {
+      icon: SlBadge,
+      title: ["전문 자격"],
+      description: "대한건축사협회 정회원",
+    },
+    {
+      icon: IoMdTime,
+      title: ["풍부한", "경험"],
+      description: "경력 20년 이상",
+    },
   ];
 
   return (
