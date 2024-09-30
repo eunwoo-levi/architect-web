8096d8ca6ccbd2530d8ddff22bfaa8cb248ee05f
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-01 01:49:52 +0900

    feat:note for pages that are not made yet

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <main className="w-full min-h-screen flex justify-center items-center font-bold">
+      아직 이 페이지는 개발중입니다.
+    </main>
+  );
 }
