ada264292849c41c33fbb0e4b8f3b98cc79de2f6
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-27 19:32:45 +0900

    feat: implement project page

---
+++ src/app/projects/data.ts
@@ -0,0 +1,11 @@
+export const data = [
+    {title:'대구 체육중학교', imageUrl:'/projects/school/cheyuk.webp'},
+    {title:'대구 두산 초등학교', imageUrl:'/projects/school/dusan.webp'},
+    {title:'대구 성주 어린이집', imageUrl:'/projects/school/seongju.webp'},
+    {title:'대구 시지 중학교', imageUrl:'/projects/school/siji.webp'},
+    {title:'고령 대창 양로원', imageUrl:'/projects/etc/goryeong.webp'},
+    {title:'고령 신협', imageUrl:'/projects/etc/goryeong-bank.webp'},
+    {title:'대구 대명동 정형 외과', imageUrl:'/projects/etc/daemyeong-hospital.webp'},
+    {title:'대구 라이온즈파크', imageUrl:'/projects/etc/lions-park.webp'},
+    {title:'성주 댐 관리소', imageUrl:'/projects/etc/seongju-dam.webp'},
+]
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,17 @@
+import ProjectLink from './_components/ProjectLink';
+import { data } from './data';
+
+interface ProjectProps {
+  title: string;
+  imageUrl: string;
+}
+
 export default function page() {
   return (
-    <main className="w-full min-h-screen flex justify-center items-center font-bold">
-      아직 이 페이지는 개발중입니다.
+    <main className='w-full grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 justify-center px-2 md:px-8 mt-[80px] mb-[100px]'>
+      {data.map((project: ProjectProps) => (
+        <ProjectLink title={project.title} imageUrl={project.imageUrl} />
+      ))}
     </main>
   );
 }
