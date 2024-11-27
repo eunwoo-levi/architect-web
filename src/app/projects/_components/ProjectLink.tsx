31b7bda2ec4f6307a3486cd16fd13b933845ef26
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-27 21:59:53 +0900

    feat: init for project detail page

--- src/app/projects/_components/ProjectLink.tsx
+++ src/app/projects/_components/ProjectLink.tsx
@@ -10,7 +10,7 @@ export default function ProjectLink({ title, imageUrl }: ProjectProps) {
   return (
     <Link
       href={`/projects/${title}`}
-      className='relative hover:z-10 group flex flex-col w-[280px] h-[280px] lg:w-[400px] lg:h-[400px] hover:scale-110 transform duration-200'
+      className='relative hover:z-10 group flex flex-col w-[280px] h-[280px] lg:w-[400px] lg:h-[400px] hover:scale-105 hover:ring-4 ring-amber-700 transform duration-200'
     >
       <Image src={imageUrl} alt='project' fill className='object-cover rounded-sm' />
       <div className='absolute bottom-0 left-0 right-0 text-center font-bold text-white bg-black/30 py-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200'>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,7 +6,7 @@ interface ProjectProps {
   imageUrl: string;
 }
 
-export default function page() {
+export default function ProjectPage() {
   return (
     <main className='w-full grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 justify-center px-2 md:px-8 mt-[80px] mb-[100px]'>
       {data.map((project: ProjectProps) => (
