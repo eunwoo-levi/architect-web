802ce3313704b2fa633c4c8a7502d76eb9dcf49a
eunwoo-levi <eunwoo1341@gmail.com>
2026-04-28 10:25:55 +0900

    feat: project에서 대구 삼성 라이온즈 강조

--- src/app/projects/data.ts
+++ src/app/projects/data.ts
@@ -1,4 +1,5 @@
 export const data = [
+  { title: "대구 라이온즈파크", imageUrl: "/projects/etc/lions-park.webp", category: "문화·레저" },
   { title: "대구 체육중학교", imageUrl: "/projects/school/cheyuk.webp", category: "교육 시설" },
   { title: "대구 두산 초등학교", imageUrl: "/projects/school/dusan.webp", category: "교육 시설" },
   { title: "대구 성주 어린이집", imageUrl: "/projects/school/seongju.webp", category: "교육 시설" },
@@ -6,6 +7,5 @@ export const data = [
   { title: "고령 대창 양로원", imageUrl: "/projects/etc/goryeong.webp", category: "복지 시설" },
   { title: "고령 신협", imageUrl: "/projects/etc/goryeong-bank.webp", category: "상업 시설" },
   { title: "대구 대명동 정형외과", imageUrl: "/projects/etc/daemyeong-hospital.webp", category: "의료 시설" },
-  { title: "대구 라이온즈파크", imageUrl: "/projects/etc/lions-park.webp", category: "문화·레저" },
   { title: "성주 댐 관리소", imageUrl: "/projects/etc/seongju-dam.webp", category: "공공 시설" },
 ];
